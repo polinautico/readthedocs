3de620acd08198d59400042f02d27c751089dabf
Lucas Martim <luomartim@gmail.com>
2023-03-13 21:15:06 -0300

    Atualizando as informações do site

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NautiDocs'
+copyright = '2023, Poli Náutico'
+author = 'Equipe Poli Náutico'
 
 release = '0.1'
 version = '0.1.0'
